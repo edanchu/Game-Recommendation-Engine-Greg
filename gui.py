40e855100a4369a74539846f025e36ba635bff85
gevorgakopyan <gevorgakopyan01@gmail.com>
2023-06-04 10:25:29 -0700

    Accept HTML

--- gui.py
+++ gui.py
@@ -7,23 +7,24 @@ import pickle
 from html.parser import HTMLParser
 import CreateGameDB as game
 import pandas as pd
+from tkhtmlview import HTMLLabel
 
-class HTMLStripper(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.fed = []
+# class HTMLStripper(HTMLParser):
+#     def __init__(self):
+#         super().__init__()
+#         self.reset()
+#         self.fed = []
 
-    def handle_data(self, data):
-        self.fed.append(data)
+#     def handle_data(self, data):
+#         self.fed.append(data)
 
-    def get_data(self):
-        return ''.join(self.fed)
+#     def get_data(self):
+#         return ''.join(self.fed)
 
-def strip_html(html):
-    html_stripper = HTMLStripper()
-    html_stripper.feed(html)
-    return html_stripper.get_data()
+# def strip_html(html):
+#     html_stripper = HTMLStripper()
+#     html_stripper.feed(html)
+#     return html_stripper.get_data()
 
 class GREG:
     def __init__(self, root, pickle_file):
@@ -66,27 +67,33 @@ class GREG:
         self.imgLabel.pack(side=TOP)
 
         self.name = self.get_game_name(self.data[self.dataIndex])
-        self.nameLabel = Label(self.center_frame, text=self.name)
+        self.nameLabel = Label(self.center_frame, text=self.name, font=("Helvetica", 16))
         self.nameLabel.pack(side=TOP)
 
         self.developer = self.get_game_developer(self.data[self.dataIndex])
-        self.developerLabel = Label(self.center_frame, text="Developer: " + self.developer)
+        self.developerLabel = Label(self.center_frame, text="Developer: " + self.developer, font=("Helvetica", 14))
         self.developerLabel.pack(side=TOP)
 
         self.publisher = self.get_game_publisher(self.data[self.dataIndex])
-        self.publisherLabel = Label(self.center_frame, text="Publisher: " + self.publisher)
+        self.publisherLabel = Label(self.center_frame, text="Publisher: " + self.publisher, font=("Helvetica", 14))
         self.publisherLabel.pack(side=TOP)
+
         self.positiveReviews = self.get_positive_review_percent(self.data[self.dataIndex])
-        self.positiveReviewsLabel = Label(self.center_frame, text="Positive Reviews: " + str(self.positiveReviews) + "%")
+        self.positiveReviewsLabel = Label(self.center_frame, text="Positive Reviews: " + str(self.positiveReviews) + "%", font=("Helvetica", 14))
         self.positiveReviewsLabel.pack(side=TOP)
+
+        spacer = Label(self.center_frame, height=1)
+        spacer.pack(side=TOP)
 
         scroll = Scrollbar(self.center_frame)
         scroll.pack(side=RIGHT, fill=Y)
 
-        self.descriptionText = Text(self.center_frame, wrap=WORD, yscrollcommand=scroll.set, height= 7, width = 70)
         self.description = self.get_game_description(self.data[self.dataIndex])
-        self.descriptionText.insert(INSERT, self.description)
+        self.descriptionText = HTMLLabel(self.center_frame, html= self.description, wrap=WORD, yscrollcommand=scroll.set, height= 15, width = 100)
         self.descriptionText.pack(side=TOP)
+
+        spacer = Label(self.center_frame, height=1)
+        spacer.pack(side=TOP)
 
         self.refresh_frame = Frame(self.root)
         self.refresh_frame.pack(side=TOP, expand=True, fill=BOTH)
@@ -96,7 +103,7 @@ class GREG:
         self.text_below_button = Label(self.refresh_frame, text = "Generating this might take a few seconds")
 
 
-        self.stars = [Button(stars_frame, text=" ★ ", fg="grey") for i in range(5)]
+        self.stars = [Button(stars_frame, text=" ★ ", fg="grey",width= 3, height=2) for i in range(5)]
         for i in range(5):
             self.stars[i].pack(side=LEFT, padx=5)
             self.stars[i].bind("<Enter>", lambda e, i=i: self.hover(i))
@@ -106,7 +113,7 @@ class GREG:
     def get_game_name(self, app_id):
         return self.gameInfo[app_id]['name']
     def get_game_description(self, app_id):
-        return strip_html(self.gameInfo[app_id]['about_the_game'])
+        return self.gameInfo[app_id]['about_the_game']
     
     def get_game_developer(self, app_id):
         return ', '.join(self.gameInfo[app_id]["developers"])
@@ -149,7 +156,7 @@ class GREG:
 
     def click(self, star_num):
         self.choice_counter += 1
-        if self.choice_counter >= 3:
+        if self.choice_counter >= 5:
             self.refresh_frame.pack()
             self.text_above_button.pack()
             self.refresh_button.pack()
@@ -176,7 +183,7 @@ class GREG:
 
         self.descriptionText.delete('1.0', END) 
         self.description = self.get_game_description(self.data[self.dataIndex])
-        self.descriptionText.insert(INSERT, self.description)
+        self.descriptionText.set_html(self.description)
         self.descriptionText.pack(side=TOP)
         self.descriptionText.config(state= DISABLED)
 
@@ -194,7 +201,7 @@ class GREG:
 def main():
     root = Tk()
     root.minsize(150,100)
-    root.maxsize(550,1000)
+    root.maxsize(1280,720)
     steamInfo = "Data\GameDictRaw.pkl"
     GREG(root, steamInfo)
     root.mainloop()
